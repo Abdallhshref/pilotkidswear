7bb62d0b0ee4d4a0305303cc80fb3d09926216be
Sonic <abdallhshref4@gmail.com>
2026-01-08 18:33:59 +0200

    Fix deployment: enable media serving and include images

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = i18n_patterns(
     path('pilotkidwear/', admin.site.urls),
@@ -30,4 +31,5 @@ urlpatterns = i18n_patterns(
 
 urlpatterns += [
     path('i18n/', include('django.conf.urls.i18n')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
