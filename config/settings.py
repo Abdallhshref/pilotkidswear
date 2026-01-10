f36bdb70e432532ef8ace4dee0cf15f4406149f2
Sonic <abdallhshref4@gmail.com>
2026-01-10 17:50:52 +0200

    last

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = 'django-insecure-v5&$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False   
 
 # settings.py
 ALLOWED_HOSTS = [
